a57688d680915890dce9c37a0013a2aaee0f4ce3
Nouchi1113 <34810143+Nouchi1113@users.noreply.github.com>
2020-03-30 16:23:24 +0900

    完成

--- Assets/MyScript/PlayerController.cs
+++ Assets/MyScript/PlayerController.cs
@@ -13,6 +13,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if(Input.GetKeyDown(KeyCode.LeftArrow))
         {
             transform.Translate(-3, 0, 0);
@@ -23,4 +24,14 @@ public class PlayerController : MonoBehaviour
         
         }
     }
+
+     public void LButtonDown()
+        {
+            transform.Translate(-3, 0, 0);
+        }
+     public void RButtonDown() 
+        {
+            transform.Translate(3, 0, 0);
+        
+        }
 }
